fe75d67a00e6841658c3d5d9a066489fec7a5c3c
Tamar  khuskivadze <86792404+Khuskivadze@users.noreply.github.com>
2022-04-01 21:50:35 +0400

    add new version

--- edu.js
+++ edu.js
@@ -1,3 +1,10 @@
-document.querySelector('#menu-btn').onclick =() =>{
-    
+let navbar = document.querySelector('.header .navbar')
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.add('active');
 }
+
+document.querySelector('#close-navbar').onclick = () =>{
+  navbar.classList.remove('active');
+};
+  
