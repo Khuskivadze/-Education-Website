559684b80a7d96679a447fd35af4d1c76630e816
Tamar  khuskivadze <86792404+Khuskivadze@users.noreply.github.com>
2022-04-04 19:01:49 +0400

    header is done

--- edu.js
+++ edu.js
@@ -9,22 +9,29 @@ document.querySelector('#close-navbar').onclick = () =>{
 };
 
 
- let registerBtn = document.querySelector ('.account-form .register-btn');
- let loginBtn = document.querySelector ('.account-form .login-btn');
+let registerBtn = document.querySelector('.account-form .register-btn');
+let loginBtn = document.querySelector('.account-form .login-btn');
 
-
- registerBtn.onclick = () =>{
-   registerBtn.classList.add('active');
-   loginBtn.classList.remove('active');
-  document.querySelector('.account-form .login-form').classList.remove('active');
-  document.querySelector('.account-form .register-form').classList.add('active');
-
- }
-
- loginBtn.onclick = () =>{
+registerBtn.onclick = () =>{
   registerBtn.classList.add('active');
   loginBtn.classList.remove('active');
- document.querySelector('.account-form .login-form').classList.add('active');
- document.querySelector('.account-form .register-form').classList.remove('active');
+  document.querySelector('.account-form .login-form').classList.remove('active');
+  document.querySelector('.account-form .register-form').classList.add('active');
+};
 
+loginBtn.onclick = () =>{
+  registerBtn.classList.remove('active');
+  loginBtn.classList.add('active');
+  document.querySelector('.account-form .login-form').classList.add('active');
+  document.querySelector('.account-form .register-form').classList.remove('active');
+};
+
+let accountform = document.querySelector('.account-form')
+
+document.querySelector('#account-btn').onclick = () =>{
+  accountform.classList.add('active');
 }
+
+document.querySelector('#close-form').onclick = () =>{
+  accountform.classList.remove('active');
+};
