55ee8ee34687ee67743024c231dc1cb1ae364212
Tamar  khuskivadze <86792404+Khuskivadze@users.noreply.github.com>
2022-04-16 22:11:21 +0400

    last version

--- edu.js
+++ edu.js
@@ -98,11 +98,21 @@ var swiper = new Swiper(".reviews-slider", {
     },
   },
 });
+ 
+
+let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
+
+accordion.forEach(acco =>{
+  acco.onclick = () =>{
+    accordion.forEach(dion => dion.classList.remove('active'));
+    acco.classList.toggle('active');
+  };
+});
 
 document.querySelector ('.load-more .btn').onclick = () => {
-   document.querySelectorAll('.course .box-container .hide').forEach( show =>{
-     show.style.display = 'block';
+  document.querySelectorAll('.course .box-container .hide').forEach( show =>{
+    show.style.display = 'block';
 
-   });
-   document.querySelector ('.load-more .btn').style.display = 'none';
+  });
+  document.querySelector ('.load-more .btn').style.display = 'none';
 };
